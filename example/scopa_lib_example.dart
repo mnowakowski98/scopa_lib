6497912d5491a6734be0ffef2e4bdc5488851466
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-05 10:41:16 -0400

    Scaffold dart package

---
+++ example/scopa_lib_example.dart
@@ -0,0 +1,6 @@
+import 'package:scopa_lib/scopa_lib.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/scopa_lib.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/scopa_lib_base.dart';
+export 'src/deck.dart';
+
+// TODO: Export any libraries intended for clients of this package.
