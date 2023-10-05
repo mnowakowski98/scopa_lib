09d570be35bc59b0efb2ad2bd6e30f45cf32f313
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-05 15:14:10 -0400

    Added doc comments to Team

--- lib/src/teams.dart
+++ lib/src/teams.dart
@@ -1,7 +1,14 @@
+/// Types for tracking [Team]s and the players in them
+library;
+
+/// A collection of players and a shared [score]
 interface class Team {
   int _score = 0;
+
+  /// The current score for the [Team].
   int get score => _score;
 
+  /// Add to the [Team]s current [score].
   bool incrementScore(int by) {
     _score += by;
     return _score >= 11;
