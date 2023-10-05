39aa455ac73b48a8edf36a122e38c1321e107d2e
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-05 15:30:33 -0400

    Add tests for unique players

--- lib/src/teams.dart
+++ lib/src/teams.dart
@@ -1,12 +1,18 @@
 /// Types for tracking [Team]s and the players in them
 library;
 
+interface class Player {
+  final String name;
+  Player(this.name);
+}
+
 /// A collection of players and a shared [score]
-interface class Team {
+class Team {
   int _score = 0;
 
   /// The current score for the [Team].
   int get score => _score;
+  var players = <Player>{};
 
   /// Add to the [Team]s current [score].
   bool incrementScore(int by) {
--- test/teams_test.dart
+++ test/teams_test.dart
@@ -3,6 +3,18 @@ import 'package:test/test.dart';
 
 void main() {
   group('Teams', () {
+    test('should contain a group of unique player', () {
+      final team = Team();
+      team.players.addAll([
+        Player('test'),
+        Player('test2'),
+        Player('test2'),
+        Player('test3'),
+      ]);
+
+      expect(team.players.length, equals(3));
+    });
+
     test('should be able to increment score', () {
       final team = Team();
       team.incrementScore(4);
@@ -18,6 +30,15 @@ void main() {
       final team3 = Team();
       expect(team3.incrementScore(5), isFalse);
       expect(team3.incrementScore(7), isTrue);
+    });
+  });
+
+  group('Players', () {
+    test('should be unique by name', () {
+      final player1 = Player('test');
+      final player2 = Player('test');
+
+      expect(player1, equals(player2));
     });
   });
 }
