4b3f764ada5152614c52e16d4ea2cd3a7e9760e1
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-10 17:43:48 -0400

    Comments

--- lib/src/table.dart
+++ lib/src/table.dart
@@ -1,16 +1,19 @@
 import 'package:scopa_lib/scopa_lib.dart';
 
+/// A seat that optionally has a [player]
 interface class Seat {
   Player? player;
   Seat();
   Seat.player(this.player);
 }
 
+/// A collection of [Seat]s and 2 [Hand]s
 class Table {
   final pool = Hand();
   final round = Hand();
 
   late final List<Seat> seats;
+
   Table(int numSeats, HandManager manager) {
     seats = List.unmodifiable(List.filled(numSeats, Seat(), growable: false));
     manager.manage(pool);
