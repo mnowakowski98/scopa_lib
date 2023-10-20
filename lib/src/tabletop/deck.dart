f44d6c1010865c7f79d59ce93e3fa1f581ec02e3
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-20 14:58:52 -0400

    Added cards getter for Deck

--- lib/src/tabletop/deck.dart
+++ lib/src/tabletop/deck.dart
@@ -11,6 +11,8 @@ class Deck {
 
   Deck(this._cards);
 
+  Set<Card> get cards => Set.unmodifiable(_cards);
+
   /// Retrieves a single [Card] by [Suite] and [value].
   Card getCard(String suite, int value) {
     final suiteCards = _cards.where((card) => card.suite == suite);
@@ -25,63 +27,5 @@ class Deck {
   /// Retrieves a set of [Card]s by [value].
   Set<Card> getValueCards(int value) {
     return _cards.where((card) => card.value == value).toSet();
-  }
-}
-
-/// A list of [Card]s that represent a players hand.
-class Hand {
-  final cards = <Card>[];
-
-  Hand();
-
-  /// Creates a [Hand] that is managed by the [manager]
-  Hand.manager(HandManager manager) {
-    manager.manage(this);
-  }
-}
-
-/// A set of [Hand]s that enforces rules between them.
-class HandManager {
-  final Deck deck;
-
-  final _hands = <Hand>{};
-
-  HandManager(this.deck);
-
-  /// Check if the given [hands] are managed.
-  void _checkManaged(List<Hand> hands) {
-    if (_hands.containsAll(hands) == false) throw ArgumentError();
-  }
-
-  /// Adds the [hand] to the set of managed [Hand]s.
-  void manage(Hand hand) {
-    _hands.add(hand);
-  }
-
-  /// Deals a [Card] to a [Hand].
-  void deal(Card card, Hand toHand) {
-    if (deck._cards.contains(card) == false) throw ArgumentError();
-    _checkManaged([toHand]);
-    for (final hand in _hands) {
-      if (hand.cards.contains(card)) {
-        move(card, hand, toHand);
-        return;
-      }
-    }
-
-    toHand.cards.add(card);
-  }
-
-  /// Removes a [Card] from a [Hand].
-  void remove(Card card, Hand fromHand) {
-    if (deck._cards.contains(card) == false) throw ArgumentError();
-    _checkManaged([fromHand]);
-    fromHand.cards.remove(card);
-  }
-
-  /// Moves a [Card] from one [Hand] another [Hand].
-  void move(Card card, Hand fromHand, Hand toHand) {
-    remove(card, fromHand);
-    deal(card, toHand);
   }
 }
--- test/tabletop/deck_test.dart
+++ test/tabletop/deck_test.dart
@@ -21,6 +21,20 @@ void main() {
       expect(card, equals(Card(suite, value)));
     });
 
+    test('can get all cards', () {
+      final deck = Deck({
+        Card('test', 5),
+        Card('test', 6),
+        Card('diamonds', 21),
+      });
+
+      final cards = deck.cards;
+      expect(cards.length, equals(3));
+      expect(cards.contains(Card('test', 5)), isTrue);
+      expect(cards.contains(Card('test', 6)), isTrue);
+      expect(cards.contains(Card('diamonds', 21)), isTrue);
+    });
+
     test('can get all cards in a suite by suite', () {
       final suite1 = 'bostoni';
       final suite2 = 'spades';
@@ -60,78 +74,4 @@ void main() {
       expect(value2Cards.every((element) => element.value == value2), isTrue);
     });
   });
-
-  // group('Hand management', () {
-  //   test('can add cards to hands', () {
-  //     final manager = HandManager(Deck());
-  //     final hand = Hand.manager(manager);
-
-  //     final card = Card(Suite.bastoni, 7);
-  //     manager.deal(Card(Suite.bastoni, 7), hand);
-
-  //     expect(hand.cards.length, equals(1));
-  //     expect(hand.cards.contains(card), isTrue);
-  //   });
-
-  //   test('can remove cards from hands', () {
-  //     final manager = HandManager(Deck());
-  //     final hand = Hand.manager(manager);
-  //     manager.deal(Card(Suite.bastoni, 7), hand);
-
-  //     final deleteCard = Card(Suite.denari, 4);
-  //     manager.deal(deleteCard, hand);
-  //     manager.remove(deleteCard, hand);
-
-  //     expect(hand.cards.length, equals(1));
-  //     expect(hand.cards.contains(deleteCard), isFalse);
-  //   });
-
-  //   test('can move cards between hands', () {
-  //     final manager = HandManager(Deck());
-  //     final card = Card(Suite.bastoni, 7);
-  //     final hand1 = Hand.manager(manager);
-  //     manager.deal(card, hand1);
-
-  //     final hand2 = Hand.manager(manager);
-  //     manager.move(card, hand1, hand2);
-  //     expect(hand1.cards.length, isZero);
-  //     expect(hand2.cards.length, equals(1));
-  //     expect(hand2.cards.contains(card), isTrue);
-  //   });
-
-  //   test('will move the card if dealt while already in a hand', () {
-  //     final manager = HandManager(Deck());
-  //     final card = Card(Suite.bastoni, 7);
-  //     final hand1 = Hand.manager(manager);
-  //     manager.deal(card, hand1);
-
-  //     final hand2 = Hand();
-  //     manager.manage(hand2);
-
-  //     manager.deal(card, hand2);
-  //     expect(hand1.cards.length, isZero);
-  //     expect(hand2.cards.length, equals(1));
-  //     expect(hand2.cards.contains(card), isTrue);
-  //   });
-
-  //   test('should throw when operating on unmanaged hands', () {
-  //     final manager = HandManager(Deck());
-
-  //     const card = Card(Suite.coppe, 4);
-  //     expect(() => manager.deal(card, Hand()), throwsArgumentError);
-  //     expect(() => manager.remove(card, Hand()), throwsArgumentError);
-  //     expect(() => manager.move(card, Hand(), Hand()), throwsArgumentError);
-  //   });
-
-  //   test('should throw if using a card not in the deck', () {
-  //     final manager = HandManager(Deck());
-
-  //     const card = Card(Suite.denari, 35);
-  //     final hand = Hand.manager(manager);
-  //     final hand2 = Hand.manager(manager);
-  //     expect(() => manager.deal(card, hand), throwsArgumentError);
-  //     expect(() => manager.remove(card, hand), throwsArgumentError);
-  //     expect(() => manager.move(card, hand, hand2), throwsArgumentError);
-  //   });
-  // });
 }
