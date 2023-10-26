8b5909bb88f1b07cf61752a8197c5156a27b7dbf
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-26 12:30:51 -0400

    Added game construction test/implementation

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -1,9 +1,28 @@
+/// Types for managing and playing the Scopa game.
+library;
+
 import 'package:scopa_lib/scopa_lib.dart';
+import 'package:scopa_lib/tabletop_lib.dart';
 
+/// Driving class for the Scopa game
 class Game {
-  final Set<Team> teams;
-  final manager = HandManager(Deck());
-  late final Table table;
+  final Set<Team> _teams;
+  late final ScopaTable table;
 
-  Game(this.teams) {}
+  Game(this._teams) {
+    final numPlayers = _teams.fold(
+        0, (previousValue, element) => previousValue += element.players.length);
+
+    table = ScopaTable(numPlayers, HandManager(ScopaDeck.instance));
+
+    var teamIndex = 0;
+    var playerIndex = 0;
+    for (final seat in table.seats) {
+      seat.player = _teams.elementAt(teamIndex).players.elementAt(playerIndex);
+      if (++teamIndex == _teams.length) {
+        teamIndex = 0;
+        playerIndex++;
+      }
+    }
+  }
 }
--- test/game_test.dart
+++ test/game_test.dart
@@ -1,18 +1,24 @@
 import 'package:scopa_lib/scopa_lib.dart';
+import 'package:scopa_lib/tabletop_lib.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Game setup', () {
-    test('assigns players around a table with striped teams', () {
-      final teams = {
-        Team.players([Player('1-1'), Player('1-2')]),
-        Team.players([Player('2-1'), Player('2-2')]),
-      };
+  group('Scopa game', () {
+    test('can be constructed with a set of teams', () {
+      final game = Game({
+        Team.players([
+          Player('1-1'),
+          Player('1-2'),
+        ]),
+        Team.players([
+          Player('2-1'),
+          Player('2-2'),
+        ])
+      });
 
-      final game = Game(teams);
-      var teamCounter = 0;
-      for (var i = 0; i < game.table.seats.length; i++) {
-        expect(game.table.seats[i].player!.name, equals('$teamCounter-$i'));
+      expect(game.table.seats.length, equals(4));
+      for (final seat in game.table.seats) {
+        expect(seat.player, isNotNull);
       }
     });
   });
