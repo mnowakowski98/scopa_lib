7c1bffcc798d42f04250e43b5c748ffcc21311ab
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-10-31 14:27:24 -0400

    Add tests for round dealing and attempted implementation

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -1,24 +1,53 @@
 /// Types for managing and playing the Scopa game.
 library;
 
+import 'dart:math';
+
 import 'package:scopa_lib/scopa_lib.dart';
 import 'package:scopa_lib/tabletop_lib.dart';
 
+/// Driving class for rounds
+class Round {
+  final Game game;
+
+  Round(this.game);
+
+  void start() {
+    final deck = ScopaDeck.instance;
+    int value() => Random().nextInt(9) + 1;
+    String suite() => deck.suites.elementAt(Random().nextInt(3));
+
+    for (var i = 0; i < 3; i++) {
+      game.manager.deal(deck.getCard(suite(), value()), game.table.round);
+    }
+
+    for (final hand in game.playerHands) {
+      for (var i = 0; i < 3; i++) {
+        game.manager.deal(deck.getCard(suite(), value()), hand);
+      }
+    }
+  }
+}
+
 /// Driving class for the Scopa game
 class Game {
+  final manager = HandManager(ScopaDeck.instance);
   final Set<Team> _teams;
   late final ScopaTable table;
+  final playerHands = <Hand>[];
 
   Game(this._teams) {
     final numPlayers = _teams.fold(
         0, (previousValue, element) => previousValue += element.players.length);
 
-    table = ScopaTable(numPlayers, HandManager(ScopaDeck.instance));
+    table = ScopaTable(numPlayers, manager);
 
     var teamIndex = 0;
     var playerIndex = 0;
     for (final seat in table.seats) {
       seat.player = _teams.elementAt(teamIndex).players.elementAt(playerIndex);
+      playerHands.add(Hand.manager(manager));
+
       if (++teamIndex == _teams.length) {
         teamIndex = 0;
         playerIndex++;
--- test/game_test.dart
+++ test/game_test.dart
@@ -21,5 +21,37 @@ void main() {
         expect(seat.player, isNotNull);
       }
     });
+
+    group('Scopa round', () {
+      test('deals 3 cards to the round hand on start', () {
+        final game = Game({});
+        final round = Round(game);
+        round.start();
+
+        expect(game.table.round.cards.length, equals(3));
+        expect(game.table.pool.cards.length, equals(37));
+      });
+
+      test('deals 3 cards to each player hand on start', () {
+        final game = Game({
+          Team.players([
+            Player('1-1'),
+            Player('1-2'),
+          ]),
+          Team.players([
+            Player('2-1'),
+            Player('2-2'),
+          ])
+        });
+
+        final round = Round(game);
+        round.start();
+
+        for (final hand in game.playerHands) {
+          expect(hand.cards.length, equals(3));
+        }
+        expect(game.table.pool.cards.length, equals(25));
+      });
+    });
   });
 }
